3821f3312f253a7e469849cf81727724fa58a974
uche <dunamishubvideo@gmail.com>
2026-06-26 02:15:26 +0100

    Correct API base URL subdomain spelling

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -7,7 +7,7 @@ import 'package:flutter/foundation.dart';
 class ApiService extends ChangeNotifier {
   static String get baseUrl {
     if (kReleaseMode) {
-      return 'https://sop.joshuageneration.com/api';
+      return 'https://sop.joshuasgeneration.com/api';
     }
     if (kIsWeb) {
       return 'http://localhost:5001/api';
